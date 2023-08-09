0049af0b39641e78f55e5c663db962253a4a30a1
UncleHanWei <a0973809807@gmail.com>
2023-08-09 23:04:39 +0800

    update direction

--- src/component/home/Directions.js
+++ src/component/home/Directions.js
@@ -16,7 +16,7 @@ function Directions() {
             <div className="modal-body text-start">
               <li><span>玩家先選擇難度(簡單、普通、困難)，困難度會影響棋盤大小</span></li>
               <li><span>遊戲一開始畫面會隨機出現一個字，玩家必須用該字造詞並進行接龍</span></li>
-              <li><span>每次造詞只能使用兩個字的詞(即輸入含題目的字之外，再加一字的詞，例如:題目為"天"，玩家則輸入 "天才")</span></li>
+              <li><span>每次造詞只能使用兩個字的詞(即除了題目的字之外，再加一字的詞，例如:題目為"天"，造詞"天才"，玩家則需要輸入 "才" 字)</span></li>
               <li><span>遊戲持續至玩家填滿棋盤(玩家勝利)或按下放棄鍵(玩家失敗)</span></li>
             </div>
             <div className="modal-footer">
