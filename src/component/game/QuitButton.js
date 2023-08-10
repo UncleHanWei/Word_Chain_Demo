76efed3085cab6407f1cc3d2e37c599d176833ba
UncleHanWei <a0973809807@gmail.com>
2023-08-10 22:05:49 +0800

    add quit confirm modal

--- src/component/game/QuitButton.js
+++ src/component/game/QuitButton.js
@@ -12,7 +12,27 @@ function QuitButton(props) {
     quitBtnRef.current.onclick = showGameover;
   }, [props])
   return (
-    <button ref={quitBtnRef} className="btn btn-success w-75" type="button">放棄</button>
+    <>
+      <button id="quit-btn" className="btn btn-success w-75" type="button" data-bs-toggle="modal" data-bs-target="#quitConfirmModal">放棄</button>
+
+      <div className="modal fade" id="quitConfirmModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content bg-dark">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Game Over</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              確定要放棄遊戲嗎?
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-primary" data-bs-dismiss="modal">繼續玩</button>
+              <button type="button" className="btn btn-danger" data-bs-dismiss="modal" ref={quitBtnRef}>放棄</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
